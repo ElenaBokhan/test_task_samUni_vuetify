70f4290fd737414c6921f1e7b6a1235ef93d0607
Elena_Bokhan <clevergirl_777@mail.ru>
2020-12-23 12:37:22 +0400

    before vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <!-- <header></header> -->
+    <main>        
+        <users />
+    </main>
+    <!-- <footer></footer> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import users from './containers/tableUsers.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: { users },    
+    methods: {
+        get(url) {
+            return fetch(url).then(d => d.json())
+        }
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>  
+.container {
+    max-width: 920px;
+    min-height: 100%;
+    margin: 0 auto;	
+    background-color: #e8e8e8;
+    display: flex;
+    flex-direction: column;		
+}
+header, footer{
+    min-height: 60px;
+    width: 100%;
+    padding: 20px;
+    /* display: flex;
+    justify-content: space-between; */
+    background-color: #467588;
+    flex: 0 0 auto;	
+}
+main{
+    flex: 1 0 auto;
+    display: flex;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './css/style.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch, faSort } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VueLodash from 'vue-lodash'
+import lodash from 'lodash'
+ 
+Vue.use(VueLodash, { name: 'custom' , lodash: lodash })
+
+library.add(faSearch)
+library.add(faSort)
+
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
